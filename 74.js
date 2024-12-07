380a8a9f170a02fb1adfd6b5e1f845e0f7de68d8
alekcandr1 <alekcandrmain@gmail.com>
2024-12-07 10:07:16 +0300

    73

---
+++ 74.js
@@ -0,0 +1,13 @@
+function removeEveryOther(arr) {
+    const newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        if (i%2 === 0) {
+            newArr.push(arr[i])
+        }
+    }
+    return newArr
+}
+
+console.log(!2%2)
+
+console.log(removeEveryOther(['Hello', 'Goodbye', 'Hello Again']))
