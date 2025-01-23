dcbdcfe25e4a0e20ecf1c4e19331a867993e26a6
alekcandr1 <alekcandrmain@gmail.com>
2025-01-23 13:33:54 +0300

    88

---
+++ 88.js
@@ -0,0 +1,21 @@
+var humanYearsCatYearsDogYears = function (humanYears) {
+    let catYears = 0;
+    let dogYears = 0;
+
+    if (humanYears >= 1) {
+        catYears += 15;
+        dogYears += 15;
+    }
+
+    if (humanYears >= 2) {
+        catYears += 9;
+        dogYears += 9;
+    }
+
+    if (humanYears > 2) {
+        catYears += 4 * (humanYears - 2);
+        dogYears += 5 * (humanYears - 2);
+    }
+
+    return [humanYears, catYears, dogYears];
+}
