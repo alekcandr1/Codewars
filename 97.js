910db7b0386445fd436ee5637ac896db608ea772
alekcandr1 <alekcandrmain@gmail.com>
2025-02-26 14:22:00 +0300

    97

---
+++ 97.js
@@ -0,0 +1,23 @@
+const db = {
+    english: "Welcome",
+    czech: "Vitejte",
+    danish: "Velkomst",
+    dutch: "Welkom",
+    estonian: "Tere tulemast",
+    finnish: "Tervetuloa",
+    flemish: "Welgekomen",
+    french: "Bienvenue",
+    german: "Willkommen",
+    irish: "Failte",
+    italian: "Benvenuto",
+    latvian: "Gaidits",
+    lithuanian: "Laukiamas",
+    polish: "Witamy",
+    spanish: "Bienvenido",
+    swedish: "Valkommen",
+    welsh: "Croeso"
+};
+
+function greet(language) {
+    return db[language] || db.english
+}
