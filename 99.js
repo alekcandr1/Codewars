10212bfe7093910740e1cad14b7c2b1e979879b4
alekcandr1 <alekcandrmain@gmail.com>
2025-03-04 13:28:23 +0300

    99

---
+++ 99.js
@@ -0,0 +1,13 @@
+function howMuchILoveYou(nbPetals) {
+    const phrases = [
+        "I love you",
+        "a little",
+        "a lot",
+        "passionately",
+        "madly",
+        "not at all"
+    ];
+
+    const index = (nbPetals - 1) % phrases.length;
+    return phrases[index];
+}
