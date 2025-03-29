c05db3c55e6e7779d7645a508c6b01e5a4e8afa9
alekcandr1 <alekcandrmain@gmail.com>
2025-03-29 11:59:53 +0300

    113

---
+++ 113.js
@@ -0,0 +1,10 @@
+function expressionMatter(a, b, c) {
+    const answers = []
+    answers.push(a * (b + c))
+    answers.push(a * b * c)
+    answers.push(a + b * c)
+    answers.push((a + b) * c)
+    return Math.max.apply(null, answers);
+}
+
+console.log(expressionMatter(3, 5, 7))
